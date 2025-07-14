b9f81db48ebcef77f7ef180a5366e029f29419cb
Reynato Muliawan <reynatomuliawan@gmail.com>
2025-07-15 03:10:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-        alias: {
-            "~": resolve(__dirname, "./app"),
-            "/css": resolve(__dirname, "./public/css"),
-        },
-    },
+ 
 })
